82752b7f5ea24c7177d450dd2ebd23c60e577cc4
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-02-19 20:23:24 -0500

    Updated for video, take 2.

--- src/query.c
+++ src/query.c
@@ -1,5 +1,10 @@
 /*
  * A demonstration of the minheap data structure
+ *
+ * Accepts and integer k <= 9 and prints the k
+ * smallest elements from the array, in sorted
+ * order.
+ *
  * Copyright Douglas B. Rumbaugh, 2022
  */
 
@@ -9,14 +14,15 @@
 int main(int argc, char *argv[])
 {
     size_t x[9] = {6, 9, 1, 2, 3, 5, 8, 15, 11};
+    size_t k = atoi(argv[1]);
 
-    minheap *heap = create_heap(10);
+    minheap *heap = create_heap(15);
 
     for (size_t i=0; i<9; i++) {
         insert_heap(heap, x[i]);
     }
 
-    for (size_t i=0; i<9; i++) {
+    for (size_t i=1; i<=k; i++) {
         size_t val;
         remove_heap(heap, &val);
 
